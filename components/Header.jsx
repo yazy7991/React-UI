604481a7b988ff9b1cb199232cfd8d97b1722486
yazy7991 <jj3369155@gmail.com>
2025-09-13 23:30:19 +0100

    Fix(Component): Fix header component style

---
+++ components/Header.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className=' bg-gray-400 px-2 py-4 h-screen '>
+        <div className='bg-amber-800'>
+            <p>logo</p>
+        </div>
+        <ul className=' bg-amber-200'>
+            <li>Home </li>
+            <li>About Us</li>
+            <li>Testimonial</li>
+            <li>Home</li>
+            <li>Home</li>
+        </ul>
+      
+    </div>
+  )
+}
+
+export default Header
