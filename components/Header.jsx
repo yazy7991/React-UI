d255844633fc7923f6c49c5186a365bf9a75f6b4
yazy7991 <jj3369155@gmail.com>
2025-09-14 13:25:52 +0100

    Add assets foder for project

--- components/Header.jsx
+++ components/Header.jsx
@@ -2,18 +2,17 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div className=' bg-gray-400 px-2 py-4 h-screen '>
-        <div className='bg-amber-800'>
+    <div className='flex gap-20 bg-gray-600 px-2 py-4 h-[90vh]'>
+        <div className='bg-amber-800 w-[20%], h-[10%]'>
             <p>logo</p>
         </div>
-        <ul className=' bg-amber-200'>
+        <ul className='flex justify-end space-x-2 w-[80%] h-[10%] bg-amber-200'>
             <li>Home </li>
             <li>About Us</li>
             <li>Testimonial</li>
-            <li>Home</li>
-            <li>Home</li>
+            <li>Services</li>
+            <li>Listing</li>
         </ul>
-      
     </div>
   )
 }
