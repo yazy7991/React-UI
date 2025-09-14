5b5e4b6b1e5b693da2b9c9dbcf98fe0ab6a6dc0c
yazy7991 <jj3369155@gmail.com>
2025-09-14 18:37:04 +0100

    Feat(component): Add navbar component

---
+++ components/Navbar.jsx
@@ -0,0 +1,75 @@
+import React, { useState } from 'react';
+import logo from '../src/assets/logo.png';
+import {navItems} from '../src/constants/index';
+import { Menu, X } from 'lucide-react';
+
+const Navbar = () => {
+    const [mobileDrawerOpen, setMobileDrawerOpen] = useState(false);
+
+    const toggleNavbar = () =>{
+        setMobileDrawerOpen(!mobileDrawerOpen);
+
+    }
+
+  return (
+    <nav className='sticky top-0 z-50 py-3 backdrop-blur-lg border-b border-neutral-700/80'>
+      <div className='container px-4 mx-auto relative text-sm'>
+        <div className='flex justify-between items-center'>
+            <div className='flex items-center flex-shrink-0'>
+                <img className='h-10 w-10 mr-2' src={logo} />
+                <span className='text-xl tracking-wide'>Gradify</span>
+            </div>
+            <ul className='hidden lg:flex ml-14 space-x-12'>
+                {navItems.map((item, index) => (
+                <li key={index}>
+                    <a href={item.href}>{item.label}</a>
+                </li>
+             ))}
+            </ul>
+
+            <div className='hidden lg:flex justify-center items-center space-x-12'>
+                <a href='#' className='py-2 px-3 border rounded-md'>
+                    Sign In
+                </a>
+                <a href='#' className='bg-gradient-to-r from-orange-500 to-orange-800 py-2 px-3 rounded-md'>
+                    Create an account
+                </a>
+            </div>
+
+            <div className='lg:hidden md:flex flex-col justify-end'>
+                <button onClick={toggleNavbar}>
+                    {mobileDrawerOpen ? <X /> : <Menu />}
+                </button>
+            </div>
+        </div>
+
+        {mobileDrawerOpen && (
+            <div className='fixed right-0 z-20 bg-neutral-900 w-full
+            p-12 flex flex-col justify-center items-center lg:hidden'>
+                <ul>
+                    {navItems.map((item, index) => (
+                        <li key={index} className='py-4'>
+                            <a href={item.href}>{item.label}</a>
+                        </li>
+                     ))}
+                    
+                </ul>
+                <div className='flex space-x-4'>
+                    <a href='#' className='py-2 px-3 border rounded-md'>
+                    Sign In
+                    </a>
+                    <a href='#' className='bg-gradient-to-r from-orange-500 to-orange-800 py-2 px-3 rounded-md'>
+                    Create an account
+                    </a>
+
+                </div>
+
+            </div>
+        )}
+
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import Header from '../components/Header';
+import Navbar from '../components/Navbar';
 
 const App = () => {
   return (
     <>
-      <Header />
+      <Navbar />
       
     </>
   )
