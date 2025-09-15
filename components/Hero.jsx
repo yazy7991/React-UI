c926b629a86ce0de608fa69327fc573eacd20b20
yazy7991 <jj3369155@gmail.com>
2025-09-15 20:18:59 +0100

    Feat(component): Add hero component

---
+++ components/Hero.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div className='text-9xl'>
+        <h1> This is the hero section</h1>
+      
+    </div>
+  )
+}
+
+export default Hero
--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from '../components/Header';
 import Navbar from '../components/Navbar';
+import Hero from '../components/Hero';
 
 const App = () => {
   return (
     <>
       <Navbar />
+      <Hero />
       
     </>
   )
